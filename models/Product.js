ad32d121ec36fa6819a2a56f4199759ea4ef27a6
Meek Meek <haggins.jameka@gmail.com>
2026-03-29 18:27:27 -0400

    Building out the Product schema, it's like clockwork at this point.

---
+++ models/Product.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: true
+  },
+  description: {
+    type: String, 
+    required: true
+  },
+  price: {
+    type: Number, 
+    required: true,
+    min: 0.01
+  },
+  category: {
+    type: String, 
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
